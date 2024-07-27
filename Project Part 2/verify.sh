627260763247aaf5b3fffdf67a437f0d5dcb77c0
“AdityaNeeraje” <aditya.neeraje@gmail.com>
2024-07-27 23:11:05 +0530

    Fina

--- Project Part 2/verify.sh
+++ Project Part 2/verify.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-for i in {1..1000};
+for i in {250..1};
 do
     random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $i | head -n 1)
-    result=$(py RSA_parity_attack.py $random_string)
-    if [[ ! py RSA_parity_attack.py $random_string ]]; then
-        
-        break
+    result=$(python3 RSA_parity_attack.py $random_string)
+    exit_code=$?
+    if [ $exit_code -eq 1 ]; then
+        echo "Error: "
+        echo $(tail -4 <<< $result)
     fi
 done
